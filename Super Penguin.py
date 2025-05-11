d4d1f5a0aefdd86b0e614b2539e0410687071417
Saadisalive <snafiullah@gmail.com>
2025-05-11 19:28:47 +0500

    Add files via upload

---
+++ Super Penguin.py
@@ -0,0 +1,27 @@
+class bird:
+    
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+    
+    def swim(self):
+        print("Siwm faster")
+
+class Penguin(bird):
+
+    def __init__(self):
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+    
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
